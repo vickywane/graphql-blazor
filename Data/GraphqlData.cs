7524090dff0349cbc2d1777bfce478e8e7b41228
victory nwani <vickywane@gmail.com>
2021-08-03 12:00:28 +0100

    update

--- Data/GraphqlData.cs
+++ Data/GraphqlData.cs
@@ -2,19 +2,33 @@ using System.Collections.Generic;
 
 namespace graphql_blazor.Data
 {
+    public class CreateCountry
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string dateInserted { get; set; }
+        public string code { get; set; }
+    } 
+
     public class GraphqlData
     {
-        List<Continents> continents { get; set; }
-    }
-
-    class Continents
-    {
-        public string name { get; set; }
         public List<Countries> countries { get; set; }
     }
 
-    class Countries
+    public class Countries
+    { 
+        public string name { get; set; }
+        public int id { get; set; }
+        public string dateInserted { get; set; }
+        public int code { get; set; }
+
+        public List<State> states { get; set; }
+    }
+
+    public class State
     {
+        public int id { get; set; }
+
         public string name { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ namespace graphql_blazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+           services.AddSingleton<GraphqlService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
